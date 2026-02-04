cea95e51f67581c55325dbb26186d52905563830
Deepak Kumar (LTIMindtree Limited) <v-deepkumar@microsoft.com>
2026-02-03 21:20:17 -0800

    commit via vs code

---
+++ day-01/myfirst.py
@@ -0,0 +1,50 @@
+print ("hello deepak")
+
+print ("hello","deepak") #print in same line
+
+print ("hello")
+print ("deepak")
+
+print(23)
+print(23+24)  
+
+
+#variables defining
+
+age=23
+name="deepak"   #to declare sring in  variable use double quotes
+price=100.98
+age1=100
+
+print(age,age1,name,price)
+
+print(name)  # print variable value
+print("name") # print stirng so use double quote
+
+print("my name is :", name)
+
+name="deepak"
+age=23
+price=100.98
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+a=50
+b=30
+
+print("and operator", (a==b)and(a>b))  #logical operator
+print("or Operator",  (a==b)or(a>b)) #logical operator
+
+name = input("Enter your name: ")  #input function
+print("Hello", name)
+val = input("Enter a value: ")  #input function
+print("Hello", val)
+
+
+val2= int(input("enter a value:"))#input function
+
+print("data type:", type(val2), (val2))  #to check data type of variable
+print("DATA TYPE :", type(name), (name))  #to check data type of variable
+print("data type inoutted:", type(val), (val))  #to check data type of variable
